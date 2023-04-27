d65aee4b52a7610707c14707411071b23339869e
Dagmawi Tesfaye <121132682+dagtes0276@users.noreply.github.com>
2023-04-27 07:40:39 +0100

    Update forms.py

--- Twitter_Clone/forms.py
+++ Twitter_Clone/forms.py
@@ -17,7 +17,7 @@ class TwittForm(forms.ModelForm):
     body = forms.CharField(required=True,
                            widget=forms.widgets.Textarea(
                                attrs={
-                                   "placeholder": "Enter Your Musker Twitt!",
+                                   "placeholder": "Enter Your Twitt!",
                                    "class": "form-control",
                                }
                            ),
